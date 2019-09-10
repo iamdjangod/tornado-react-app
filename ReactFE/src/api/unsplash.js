bf1fb183b34cdbbb02149fec5e0c0d39a7384fc6
Djangod <markemma2012@gmail.com>
2019-09-10 18:00:39 +0100

    Delete unsplash.js

--- ReactFE/src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-    Authorization:
-      'Client-ID 012cb8cfd5963a7c599f66e5306bc7ac2436bc3d6597718ab219b9e52494c558'
-  }
-});
